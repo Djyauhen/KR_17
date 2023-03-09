4d9d84031dc9b55a050ec2bcdd296e304a908682
Djyauhen <fursaevgenij@gmail.com>
2023-03-10 01:20:25 +0300

    full work

---
+++ index.js
@@ -0,0 +1,21 @@
+import {Worker} from "./worker.js";
+
+
+let workers = [
+    ['Иван', 'Федоров', '03.05.2001', 'Веб-разработчик', 1200, 27],
+    ['Федор', 'Иванов', '01.06.1996', 'Веб-дизайнер', 1300, 22],
+    ['Елена', 'Николаенко', '11.05.2003', 'Веб-разработчик', 1150, 15],
+    ['Павел', 'Тринда', '03.21.1989', 'Веб-разработчик', 1450, 26],
+    ['Татьяна', 'Свиридова', '05.24.2003', 'Веб-дизайнер', 2150, 10],
+]
+
+workers.forEach(item => {
+    let worker = new Worker(item[0], item[1], item[2], item[3]);
+    worker.Rate = item[4];
+    worker.Days = item[5];
+    console.log(`${worker.getFullName()}. Заработал: ${worker.getSalary()} рублей`);
+})
+
+Worker.whoWorkedMore(workers);
+Worker.whoIsYounger(workers);
+
--- person.js
+++ person.js
@@ -1,32 +1,40 @@
-class Person {
+export class Person {
 
     #birthday;
 
-    constructor(firstName, lastName) {
+    constructor(firstName, lastName, birth) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.#birthday = birth;
+        this.birthday;
     }
 
     get birthday() {
-        this.#birthday = prompt('введите дату рождения в формате "мм-дд-гггг"');
-        return this.#birthday;
-    }
-
-    set birthday(day) {
-
+        let birth = this.#birthday;
+        let partsBirth = birth.split('.');//Разделяем дату на месяц, число и год
+        if (partsBirth[0] >=1 && partsBirth[0] <=12) {
+            return this.#birthday;
+        }
     }
 
     getFullName() {
         return `${this.firstName} ${this.lastName}`;
     }
+
+    getAge() {
+        if (this.birthday) {
+            let birth = this.#birthday;
+            let partsBirth = birth.split('.');//Разделяем дату на месяц, число и год
+            let todayYear = new Date().getFullYear();
+            let age = todayYear - partsBirth[2];
+            let ageLetter = age % 10;//Остаток от деления
+            if (ageLetter >= 5 || ageLetter === 0) {
+                return `Возраст: ${age} лет`;
+            } if (ageLetter === 1) {
+                return `Возраст: ${age} год`;
+            } if (ageLetter >= 2 && ageLetter <= 4) {
+                return `Возраст: ${age} года`;
+            }
+        }
+    }
 }
-
-let friend = new Person('Ivan', 'Polovinkin');
-console.log(friend.getFullName());
-console.log(friend);
-
-//
-// let date = new Date().getMonth();
-//
-// console.log(date);
-
